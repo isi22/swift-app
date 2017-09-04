2288c13e2d4d0a85fce2eb5f912fc8f974ae985d
Isabelle <isabelle@mac-isabelle.speedport_w_921v_1_43_000>
2017-09-04 17:22:04 +0200

    Boolean

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -18,7 +18,15 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        theLabel.text = "Answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
+        let addition = false
+        
+        if addition {
+            theLabel.text = "Answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            theLabel.text = "Answer is... \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
+        
+        
         
     }
     
@@ -29,12 +37,12 @@ class ViewController: UIViewController {
         
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     } 
-
-
+    
+    
 }
 
